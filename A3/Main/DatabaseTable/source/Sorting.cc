13a348ad2ed1142d4c6036616c03886b9d2eacf7
Gabriel Ong <gabrielong2004@gmail.com>
2025-10-03 11:59:39 -0500

    Implemented mergeIntoFile

--- A3/Main/DatabaseTable/source/Sorting.cc
+++ A3/Main/DatabaseTable/source/Sorting.cc
@@ -8,9 +8,35 @@
 #include "MyDB_TableReaderWriter.h"
 #include "Sorting.h"
 
+#include <queue>
+
 using namespace std;
 
-void mergeIntoFile (MyDB_TableReaderWriter &, vector <MyDB_RecordIteratorAltPtr> &, function <bool ()>, MyDB_RecordPtr, MyDB_RecordPtr) {}
+void mergeIntoFile (MyDB_TableReaderWriter &table_rw, vector <MyDB_RecordIteratorAltPtr> &iters, function <bool ()> comparator, MyDB_RecordPtr record1, MyDB_RecordPtr record2) {
+	auto iter_comparator = [comparator, record1, record2](MyDB_RecordIteratorAltPtr iter1, MyDB_RecordIteratorAltPtr iter2) {
+		iter1->getCurrent(record1);
+		iter2->getCurrent(record2);
+
+		// comparator return true if record1 < record2, but the STL
+		// priority queue defaults to a max heap and we want a min heap.
+		return !comparator();
+	};
+
+	priority_queue<MyDB_RecordIteratorAltPtr, vector<MyDB_RecordIteratorAltPtr>, function< bool (MyDB_RecordIteratorAltPtr, MyDB_RecordIteratorAltPtr)>> pq(iters.begin(), iters.end());
+
+	while (pq.size()) {
+		MyDB_RecordIteratorAltPtr iter = pq.top();
+		pq.pop();
+		
+		MyDB_RecordPtr record = table_rw.getEmptyRecord();
+		iter->getCurrent(record);
+		table_rw.append(record);
+
+		// Push iterator back if it has more.
+		if (iter->advance())
+			pq.push(iter);
+	}
+}
 
 vector <MyDB_PageReaderWriter> mergeIntoList (MyDB_BufferManagerPtr, MyDB_RecordIteratorAltPtr, MyDB_RecordIteratorAltPtr, function <bool ()>, 
 	MyDB_RecordPtr, MyDB_RecordPtr) {return vector <MyDB_PageReaderWriter> (); } 
